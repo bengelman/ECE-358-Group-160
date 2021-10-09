fba1bad17338cc6f33cd8591fee5cb3626dc7a7d
Brendan Engelman <brendan.engelman@gmail.com>
2021-10-08 21:51:02 -0400

    Comments

--- Lab1/Lab1.java
+++ Lab1/Lab1.java
@@ -19,12 +19,17 @@ abstract class Event {
     abstract void doEventK(SortedSet<Event> allEvents, int[] queueSize, List<Integer> observedPacketsInBuffer, int[] lostPackets, double[] lastDepartureTime, int K);
 }
 
+/**
+ * Event indicating an observer recording queue state
+ */
 class ObserverEvent extends Event {
     ObserverEvent(double time){
         super(time);
     }
     void doEvent(int[] queueSize, List<Integer> observedPacketsInBuffer, List<Boolean> observedIdle){
+        // Record packets in queue
         observedPacketsInBuffer.add(queueSize[0]);
+        // Record whether queue is idle
         observedIdle.add(queueSize[0] == 0);
     }
 
@@ -33,6 +38,9 @@ class ObserverEvent extends Event {
     }
 }
 
+/**
+ * Event indicating a packet entering the queue
+ */
 class ArrivalEvent extends Event {
     double lengthRate;
     double transmissionSpeed;
@@ -51,6 +59,7 @@ class ArrivalEvent extends Event {
     }
 
     void doEvent(int[] queueSize, List<Integer> observedPacketsInBuffer, List<Boolean> observedIdle){
+        // Add packet to queue
         queueSize[0]++;
     }
 
@@ -70,11 +79,15 @@ class ArrivalEvent extends Event {
     }
 }
 
+/**
+ * Event indicating a packet leaving the queue
+ */
 class DepartureEvent extends Event {
     DepartureEvent(double time){
         super(time);
     }
     void doEvent(int[] queueSize, List<Integer> observedPacketsInBuffer, List<Boolean> observedIdle){
+        // Remove packet from queue
         queueSize[0]--;
     }
     void doEventK(SortedSet<Event> allEvents, int[] queueSize, List<Integer> observedPacketsInBuffer, int[] lostPackets, double[] lastDepartureTime, int K) {
@@ -82,6 +95,9 @@ class DepartureEvent extends Event {
     }
 }
 
+/**
+ * Allows us to sort events by execution order
+ */
 class EventComparator implements Comparator<Event> {
     public int compare(Event e1, Event e2){
         if (e1.time < e2.time){
@@ -91,6 +107,9 @@ class EventComparator implements Comparator<Event> {
     }
 }
 
+/**
+ * Structure for storing experimental results
+ */
 class ResultSet {
     double avgPackets;
     double pIdle;
@@ -115,18 +134,27 @@ public class Lab1 {
         return - (1D / lambda) * Math.log(1D - Math.random());
     }
 
+    /**
+     * Generate a list of 1000 exponential randoms and print them to file
+     * 
+     * @throws FileNotFoundException
+     */
     public static void q1() throws FileNotFoundException{
+        // Open output file
         PrintStream output = new PrintStream(new FileOutputStream(new File("q1output.txt")));
         double cumulative = 0D;
         double[] randoms = new double[1000];
         for (int i = 0; i < 1000; i++){
+            // Generate random
             randoms[i] = exponentialRandom(75);
             output.println(randoms[i]);
             cumulative += randoms[i];
         }
+        // Calculate mean
         double mean = cumulative / 1000D;
         output.println("Mean: " + mean);
         double variance = 0;
+        // Iteratively calculate variance
         for (int i = 0; i < 1000; i++){
             variance += Math.pow(randoms[i] - mean, 2);
         }
@@ -136,46 +164,74 @@ public class Lab1 {
 
     // Queston 2
 
+    /**
+     * Simulate a packet queue with the given parameters
+     * 
+     * @param totalTime
+     * @param arrivalRate
+     * @param lengthRate
+     * @param transmissionSpeed
+     * @param observerRate
+     * @return ResultSet containing average # packets in queue and idle %
+     */
     static ResultSet runSim(double totalTime, double arrivalRate, double lengthRate, double transmissionSpeed, double observerRate){
+        // List of all events, sorted by execution order using custom comparator
         SortedSet<Event> allEvents = new TreeSet<>(new EventComparator());
         List<ArrivalEvent> arrivals = new ArrayList<>();
         List<DepartureEvent> departures = new ArrayList<>();
         List<ObserverEvent> observers = new ArrayList<>();
+        // Generate first arrival time
         double arrivalTime = exponentialRandom(arrivalRate);
+        // Keep generating arrival times until we exceed the experiment time
         while(arrivalTime < totalTime){
+            // Create a new event
             ArrivalEvent arrivalEvent = new ArrivalEvent(arrivalTime);
             arrivals.add(arrivalEvent);
+            // Add to sorted list of all events
             allEvents.add(arrivalEvent);
+            // Generate the next arrival time
             arrivalTime += exponentialRandom(arrivalRate);
         }
         double lastDeparture = 0;
+        // Generate a departure for each arriving packet
         for (int i = 0; i < arrivals.size(); i++){
+            // Generate packet service time
             double transmitTime = exponentialRandom(lengthRate) / transmissionSpeed;
+            // Packet takes transmitTime to service, starting from the last departure, or the time the packet arrived, whichever is later
             lastDeparture = Math.max(lastDeparture, arrivals.get(i).time) + transmitTime;
             if (lastDeparture < totalTime){
+                // If the given time is within our range, create the event
                 DepartureEvent departureEvent = new DepartureEvent(lastDeparture);
                 departures.add(departureEvent);
+                // Add to sorted list of all events
                 allEvents.add(departureEvent);
             }
             else {
                 break;
             }
         }
+        // Generate the first observer time
         double observerTime = exponentialRandom(observerRate);
+        // Keep generating observer times until we exceed the experiment time
         while(observerTime < totalTime){
             ObserverEvent observerEvent = new ObserverEvent(observerTime);
             observers.add(observerEvent);
+            // Add to sorted list of all events
             allEvents.add(observerEvent);
+            // Generate the next observer time
             observerTime += exponentialRandom(observerRate);
         }
+        // This is really just an int, but creating as an array allows us to pass by reference
         int[] queueSize = new int[1];
         List<Integer> observedPacketsInBuffer = new ArrayList<>();
         List<Boolean> observedIdle = new ArrayList<>();
+        // Service all events, in execution order
         for (Event event : allEvents){
             event.doEvent(queueSize, observedPacketsInBuffer, observedIdle);
         }
         double totalPackets = 0;
         double totalIdle = 0;
+        // Calculate averages of total packets, idle time
         for (int i = 0; i < observedPacketsInBuffer.size(); i++){
             totalPackets += (double)observedPacketsInBuffer.get(i);
         }
@@ -230,16 +286,24 @@ public class Lab1 {
         return new ResultSet(totalPackets/(double)observedPacketsInBuffer.size(), lostPackets[0]/(double)numArrivalEvents, 0);
     }
 
+    /**
+     * Find a value of totalTime for which the experiment is stable, given a rho and infinite buffersize
+     * 
+     * @param rho
+     */
     static void stabilizeT(double rho){
         int multiplier = 1;
         ResultSet prevResults = null;
         while (true){
+            // Run sim at given time
             System.out.println(multiplier);
             ResultSet results = runSim(1000 * multiplier, rho * SPEED / LENGTH, 1D / LENGTH, SPEED, 5D * rho * SPEED/LENGTH);
             if (prevResults != null){
+                // Compare current and previous results
                 double dPackets = Math.abs((results.avgPackets - prevResults.avgPackets) / prevResults.avgPackets);
                 double dPIdle = prevResults.pIdle == 0 ? 0 : Math.abs((results.pIdle - prevResults.pIdle) / prevResults.pIdle);
                 if (prevResults != null && dPackets < 0.05 && dPIdle < 0.05){
+                    // Results are withni 5%, we have found our target
                     break;
                 }
             }
@@ -249,16 +313,25 @@ public class Lab1 {
         System.out.println("Results for rho=" + rho + " stabilize at T=" + 1000 * (multiplier - 1));
     }
 
+    /**
+     * Find a value of totalTime for which the experiment is stable, given a rho and buffersize
+     * 
+     * @param rho
+     * @param bufferSize
+     */
     static void stabilizeTK(double rho, int bufferSize){
         int multiplier = 1;
         ResultSet prevResults = null;
         while (true){
+            // Run sim at given time
             ResultSet results = runSimK(1000 * multiplier, rho * SPEED / LENGTH, 1D / LENGTH, SPEED, 5D * rho * SPEED/LENGTH, bufferSize);
             if (prevResults != null){
+                // Compare current and previous results
                 double dPackets = prevResults.avgPackets == 0 && results.avgPackets < 0.01 ? 0 : Math.abs((results.avgPackets - prevResults.avgPackets) / prevResults.avgPackets);
                 double dPIdle = prevResults.pIdle == 0 && results.pIdle < 0.01 ? 0 : Math.abs((results.pIdle - prevResults.pIdle) / prevResults.pIdle);
                 double dPLoss = prevResults.pLoss == 0 && results.pLoss < 0.01 ? 0 : Math.abs((results.pLoss - prevResults.pLoss) / prevResults.pLoss);
                 if (prevResults != null && dPackets < 0.05 && dPIdle < 0.05 && dPLoss < 0.05){
+                    // Results are withni 5%, we have found our target
                     break;
                 }
             }
@@ -268,15 +341,25 @@ public class Lab1 {
         System.out.println("Results for rho=" + rho + ", buffersize=" + bufferSize + " stabilize at T=" + 1000 * (multiplier - 1));
     }
 
+    // Average packet length, in bits
     static final double LENGTH = 2000D;
+    // Packet service speed, in bits/second
     static final double SPEED = 1000000D;
 
+    /**
+     * Generate data for question 2
+     * 
+     * @param totalTime
+     * @throws FileNotFoundException
+     */
     static void runQ2Experiment(double totalTime) throws FileNotFoundException{
         System.out.println("Q2 results with infinite buffer:");
         PrintStream output = new PrintStream(new FileOutputStream(new File("q2results.csv")));
         for (double rho = 0.25; rho <= 0.95; rho += 0.1){
+            // Run sim at given rho value
             ResultSet results = runSim(totalTime, rho * SPEED / LENGTH, 1D / LENGTH, SPEED, 5*rho*SPEED/LENGTH);
             System.out.printf("Results for rho = %.2f: avg packets %.2f, idle %.4f\n", rho, results.avgPackets, results.pIdle);
+            // Output to csv for easy chartifying
             output.printf("%.2f,%.2f,%.4f\n", rho, results.avgPackets, results.pIdle);
         }
     }
